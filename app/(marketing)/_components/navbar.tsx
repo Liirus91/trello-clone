b647ace930ed5794887645c79452ba5b4ac63707
Liirus91 <work.liirus@gmail.com>
2023-11-25 09:26:16 +0200

    feat: add sign-in and sign-up buttons

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,10 +1,20 @@
 import { Logo } from '@/components/logo';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export const Navbar = () => {
   return (
     <div className="fixed flex items-center top-0 w-full h-14 px-4 border-b shadow-sm bg-white">
       <div className="flex items-center justify-between w-full mx-auto md:max-w-screen-2xl">
         <Logo />
+        <div className="flex items-center justify-between w-full space-x-4 md:block md:w-auto">
+          <Button size="sm" variant="outline" asChild>
+            <Link href="/sign-in">Login</Link>
+          </Button>
+          <Button size="sm" asChild>
+            <Link href="/sign-up">Get TrelloClone for free</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
