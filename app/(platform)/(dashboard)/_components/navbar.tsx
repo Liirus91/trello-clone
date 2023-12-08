6af37f6ac7b12ee9199c3d98d81bf7cedc716fb7
Liirus91 <work.liirus@gmail.com>
2023-12-08 09:17:33 +0200

    feat: add buttons in navbar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,3 +1,46 @@
+import { Logo } from '@/components/logo';
+import { Button } from '@/components/ui/button';
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
+import { Plus } from 'lucide-react';
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex items-center fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white">
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+        <Button
+          size="sm"
+          className="hidden rounded-sm md:block h-auto py-1.5 px-2"
+        >
+          Create
+        </Button>
+        <Button size="sm" className="block rounded-sm md:hidden">
+          <Plus className="h-4 w-4" />
+        </Button>
+      </div>
+      <div className="flex items-center gap-x-2 ml-auto">
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl="/organization/:id"
+          afterLeaveOrganizationUrl="/select-org"
+          afterSelectOrganizationUrl="/organization/:id"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              },
+            },
+          }}
+        />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{ elements: { avatarBox: { height: 30, width: 30 } } }}
+        />
+      </div>
+    </nav>
+  );
 };
