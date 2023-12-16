e0e70d4c7b8ab62e253e426e42ea90a3eb9df81c
Liirus91 <work.liirus@gmail.com>
2023-12-16 08:46:57 +0200

    added logic and part of layouts in sidebar

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,11 +1,79 @@
 'use client';
 
+import { Accordion } from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
+import { useOrganization, useOrganizationList } from '@clerk/nextjs';
+import { Plus } from 'lucide-react';
+import Link from 'next/link';
+import { useLocalStorage } from 'usehooks-ts';
+
 interface SidebarProps {
   storageKey?: string;
 }
 
-const Sidebar = ({ storageKey }: SidebarProps) => {
-  return <div>Sidebar</div>;
+const Sidebar = ({ storageKey = 't-sidebar-state' }: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization();
+
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: { infinite: true },
+  });
+
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+
+      return acc;
+    },
+    []
+  );
+
+  const onExpend = (id: string) => {
+    setExpanded((cur) => ({ ...cur, [id]: !expanded[id] }));
+  };
+
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <Skeleton />
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="flex items-center font-medium text-xs mb-1">
+        <span className="pl-4">Workspaces</span>
+        <Button
+          asChild
+          type="button"
+          size="icon"
+          variant="ghost"
+          className="ml-auto"
+        >
+          <Link href="/select-org">
+            <Plus className="h-4 w-4" />
+          </Link>
+        </Button>
+      </div>
+      <Accordion
+        type="multiple"
+        defaultValue={defaultAccordionValue}
+        className="space-y-2"
+      >
+        {userMemberships.data.map(({ organization }) => (
+          <p key={organization.id}>{organization.id}</p>
+        ))}
+      </Accordion>
+    </>
+  );
 };
 
 export default Sidebar;
