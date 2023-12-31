70470feabe2bb6073b9c2b8cedeb2778535cb9ac
Liirus91 <work.liirus@gmail.com>
2023-12-31 08:32:15 +0200

    feat: add input title in organization page

--- app/(platform)/(dashboard)/organization/[organizationid]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationid]/page.tsx
@@ -1,5 +1,27 @@
+import { db } from '@/lib/db';
+
 const OrganizationIdPage = () => {
-  return <div>Organization Page</div>;
+  async function create(formData: FormData) {
+    'use server';
+
+    const title = formData.get('title') as string;
+
+    await db.board.create({ data: { title } });
+  }
+
+  return (
+    <div>
+      <form action={create}>
+        <input
+          id="title"
+          placeholder="Enter a board title"
+          name="title"
+          required
+          className="border p-1 border-black"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
