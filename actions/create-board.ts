45193e6b1d5480620c1e2eaeebdb0aec499ec974
Liirus91 <work.liirus@gmail.com>
2024-01-06 08:53:50 +0200

    feat: create board component

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { db } from '@/lib/db';
+import { revalidatePath } from 'next/cache';
 import { z } from 'zod';
 
 const CreateBoard = z.object({ title: z.string() });
@@ -9,4 +10,6 @@ export async function create(formData: FormData) {
   const { title } = CreateBoard.parse({ title: formData.get('title') });
 
   await db.board.create({ data: { title } });
+
+  revalidatePath('');
 }
