0676837edb3277db7ae99211bacbac8aedae2275
Liirus91 <work.liirus@gmail.com>
2024-01-10 10:09:52 +0200

    feat: show error if written not enough symbols in title field

--- app/(platform)/(dashboard)/organization/[organizationid]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationid]/form.tsx
@@ -5,18 +5,29 @@ import { Button } from '@/components/ui/button';
 import { useFormState } from 'react-dom';
 
 export const Form = () => {
-  const initialState = { message: null, errors: {} };
+  const initialState = { errors: {}, message: null };
   const [state, dispatch] = useFormState(create, initialState);
 
   return (
     <form action={dispatch}>
-      <input
-        id="title"
-        placeholder="Enter a board title"
-        name="title"
-        required
-        className="border p-1 border-black"
-      />
+      <div className="flex flex-col space-y-2">
+        <input
+          id="title"
+          placeholder="Enter a board title"
+          name="title"
+          required
+          className="border p-1 border-black"
+        />
+        {state?.errors?.title ? (
+          <div>
+            {state.errors.title.map((error: string) => (
+              <p className="text-rose-500" key={error}>
+                {error}
+              </p>
+            ))}
+          </div>
+        ) : null}
+      </div>
       <Button type="submit">Submit</Button>
     </form>
   );
