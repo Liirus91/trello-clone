707efc47cdfddc186db2eec1ec651042fee1a0f1
Liirus91 <work.liirus@gmail.com>
2024-01-27 09:44:43 +0200

    feat: add separator under info block

--- app/(platform)/(dashboard)/organization/[organizationid]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationid]/page.tsx
@@ -1,9 +1,11 @@
+import { Separator } from '@/components/ui/separator';
 import { Info } from './_components/info';
 
 const OrganizationIdPage = async () => {
   return (
     <div className="w-full mb-20">
       <Info />
+      <Separator className="my-4" />
     </div>
   );
 };
