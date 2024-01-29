e160ab34260be59c113cb43c130bf1742c2980da
Liirus91 <work.liirus@gmail.com>
2024-01-29 09:28:30 +0200

    feat: add info and button in BoardList

--- app/(platform)/(dashboard)/organization/[organizationid]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationid]/_components/board-list.tsx
@@ -1,3 +1,21 @@
+import { User2 } from 'lucide-react';
+
 export const BoardList = () => {
-  return <div>Board List</div>;
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center font-semibold text-lg text-neutral-700">
+        <User2 className="w-6 h-6 mr-2" />
+        Your boards
+      </div>
+      <div className="grid gap-4 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
+        <div
+          className="flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition aspect-video relative w-full h-full bg-muted rounded-sm"
+          role="button"
+        >
+          <p className="text-sm">Create new board</p>
+          <span className="text-xs">5 remaining</span>
+        </div>
+      </div>
+    </div>
+  );
 };
