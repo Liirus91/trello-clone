51df7d1f7e0af105f94461b03652027fe78c64a1
Liirus91 <work.liirus@gmail.com>
2024-02-02 09:49:40 +0200

    feat: use PopoverClose in FormPopover

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,6 +1,13 @@
 'use client';
 
-import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+import { X } from 'lucide-react';
+import { Button } from '../ui/button';
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from '../ui/popover';
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -27,6 +34,14 @@ export const FormPopover = ({
         <div className="text-center text-sm font-medium pb-4 text-neutral-600">
           Create board
         </div>
+        <PopoverClose asChild>
+          <Button
+            variant="ghost"
+            className="p-2 h-auto w-auto absolute top-2 right-2 text-neutral-600"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
       </PopoverContent>
     </Popover>
   );
