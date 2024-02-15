f8e3bd087ef0ccb1d1da69bd65f9e7b46ec5882b
Liirus91 <work.liirus@gmail.com>
2024-02-15 10:23:07 +0200

    feat: add enableEditing in BoardTitleForm

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -1,15 +1,58 @@
 'use client';
 
+import { FormInput } from '@/components/form/form-input';
 import { Button } from '@/components/ui/button';
 import { Board } from '@prisma/client';
+import { ElementRef, useRef, useState } from 'react';
 
 interface BoardTitleFormProps {
   data: Board;
 }
 
 export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const formRef = useRef<ElementRef<'form'>>(null);
+  const inputRef = useRef<ElementRef<'input'>>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string;
+    console.log(title);
+  };
+
+  if (isEditing) {
+    return (
+      <form
+        action={onSubmit}
+        ref={formRef}
+        className="flex items-center gap-x-2"
+      >
+        <FormInput
+          id="title"
+          onBlur={() => {}}
+          ref={inputRef}
+          defaultValue={data.title}
+          classname="bg-transparent h-7 py-1 px-[7px] font-bold text-lg focus-visible:outline-none focus-visible:ring-transparent border-none"
+        />
+      </form>
+    );
+  }
+
   return (
     <Button
+      onClick={enableEditing}
       className="h-auto w-auto p-1 px-2 font-bold text-lg"
       variant="transparent"
     >
