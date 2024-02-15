b60ba8beeb75ae513e9fecd70a5ed126c23185aa
Liirus91 <work.liirus@gmail.com>
2024-02-15 10:01:53 +0200

    feat: create BoardTitleForm and use it in Board Navbar

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,4 +1,5 @@
 import { Board } from '@prisma/client';
+import { BoardTitleForm } from './board-title-form';
 
 interface BoardNavbarProps {
   data: Board;
@@ -7,7 +8,7 @@ interface BoardNavbarProps {
 export const BoardNavbar = async ({ data }: BoardNavbarProps) => {
   return (
     <div className="flex items-center px-6 gap-x-4 text-white top-14 fixed w-full h-14 z-[40] bg-black/50">
-      Board Navbar
+      <BoardTitleForm data={data} />
     </div>
   );
 };
