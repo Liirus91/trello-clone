c96bc17e326cb28550bfda3b509d5d033677fcb9
Liirus91 <work.liirus@gmail.com>
2024-02-17 09:23:28 +0200

    feat: use updateBoard in board title form

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -1,15 +1,30 @@
 'use client';
 
+import { updateBoard } from '@/actions/update-board';
 import { FormInput } from '@/components/form/form-input';
 import { Button } from '@/components/ui/button';
+import { useAction } from '@/hooks/use-action';
 import { Board } from '@prisma/client';
 import { ElementRef, useRef, useState } from 'react';
+import { toast } from 'sonner';
 
 interface BoardTitleFormProps {
   data: Board;
 }
 
 export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
+  const { execute } = useAction(updateBoard, {
+    onSuccess: (data) => {
+      toast.success(`Board "${data.title}" updated.`);
+      setTitle(data.title);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const [title, setTitle] = useState(data.title);
   const [isEditing, setIsEditing] = useState(false);
 
   const formRef = useRef<ElementRef<'form'>>(null);
@@ -29,7 +44,8 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get('title') as string;
-    console.log(title);
+
+    execute({ title, id: data.id });
   };
 
   const onBlur = () => {
@@ -47,7 +63,7 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
           id="title"
           onBlur={onBlur}
           ref={inputRef}
-          defaultValue={data.title}
+          defaultValue={title}
           classname="bg-transparent h-7 py-1 px-[7px] font-bold text-lg focus-visible:outline-none focus-visible:ring-transparent border-none"
         />
       </form>
@@ -60,7 +76,7 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
       className="h-auto w-auto p-1 px-2 font-bold text-lg"
       variant="transparent"
     >
-      {data.title}
+      {title}
     </Button>
   );
 };
