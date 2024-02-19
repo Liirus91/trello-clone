133c58675a340d45e678d14c227ee57de4979e5f
Liirus91 <work.liirus@gmail.com>
2024-02-19 08:57:27 +0200

    feat: add props in BoardIdPage

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,5 +1,25 @@
-const BoardIdPage = () => {
-  return <div>Board Id page</div>;
+import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+interface BoardIdPageProps {
+  params: { boardId: string };
+}
+
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect('/select-org');
+  }
+
+  const lists = await db.list.findMany({
+    where: { boardId: params.boardId, board: { orgId } },
+    include: { cards: { orderBy: { order: 'asc' } } },
+    orderBy: { order: 'asc' },
+  });
+
+  return <div className="h-full p-4 overflow-x-auto">Board Id page</div>;
 };
 
 export default BoardIdPage;
