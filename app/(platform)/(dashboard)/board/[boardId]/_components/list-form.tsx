15a64fdcbe992649ad09b25d38e3edcadc7c261c
Liirus91 <work.liirus@gmail.com>
2024-02-20 09:45:01 +0200

    feat: add button Add list

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -1,7 +1,15 @@
 'use client';
 
+import { Plus } from 'lucide-react';
 import { ListWrapper } from './list-wrapper';
 
 export const ListForm = () => {
-  return <ListWrapper>List form</ListWrapper>;
+  return (
+    <ListWrapper>
+      <button className="flex items-center font-medium text-sm p-3 w-full transition rounded-md bg-white/80 hover:bg-white/50 ">
+        <Plus className="h-4 w-4 mr-2" />
+        Add list
+      </button>
+    </ListWrapper>
+  );
 };
