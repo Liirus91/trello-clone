b958d39452df17adaba9e7173fa296f3257b3838
Liirus91 <work.liirus@gmail.com>
2024-02-22 09:40:00 +0200

    feat; create ListHeader component

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -18,7 +18,7 @@ export const ListContainer = ({ boardId, data }: ListContainerProps) => {
   }, [data]);
 
   return (
-    <ol>
+    <ol className="flex gap-x-3 h-full">
       {orderData.map((list, index) => {
         return <ListItem key={list.id} index={index} data={list} />;
       })}
