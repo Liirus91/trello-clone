23f02c17db9920d1f45eca0db787b7253830a8bb
Liirus91 <work.liirus@gmail.com>
2024-02-22 09:47:42 +0200

    feat: send title to ListHeader

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,10 +1,16 @@
 'use client';
 
-export const ListHeader = () => {
+import { List } from '@prisma/client';
+
+interface ListHeaderProps {
+  data: List;
+}
+
+export const ListHeader = ({ data }: ListHeaderProps) => {
   return (
     <div className="flex justify-between items-start gap-x-2 pt-2 px-2 text-sm font-semibold">
       <div className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent">
-        List Header
+        {data.title}
       </div>
     </div>
   );
