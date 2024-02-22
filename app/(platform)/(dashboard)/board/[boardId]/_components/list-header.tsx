0824fa9e3c5c450e7c3a6ef804b58be259948280
Liirus91 <work.liirus@gmail.com>
2024-02-22 09:45:08 +0200

    feat: add layout for ListHeader

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,5 +1,11 @@
 'use client';
 
 export const ListHeader = () => {
-  return <div>List Header</div>;
+  return (
+    <div className="flex justify-between items-start gap-x-2 pt-2 px-2 text-sm font-semibold">
+      <div className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent">
+        List Header
+      </div>
+    </div>
+  );
 };
