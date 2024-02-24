ff0e8e44308f8bf9fc79bc2b18cc55c97f1439a5
Liirus91 <work.liirus@gmail.com>
2024-02-24 10:13:10 +0200

    feat: add popover in ListOptions

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -1,6 +1,14 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 import { List } from '@prisma/client';
+import { MoreHorizontal, X } from 'lucide-react';
 
 interface ListOptionsProps {
   data: List;
@@ -8,5 +16,33 @@ interface ListOptionsProps {
 }
 
 export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
-  return <div>List Options</div>;
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="w-auto h-auto p-2" variant="ghost">
+          <MoreHorizontal className="w-4 h-4" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent side="bottom" align="start" className="px-0 pt-3 pb-3">
+        <div className="text-sm font-medium text-center pb-4 text-neutral-600">
+          List actions
+        </div>
+        <PopoverClose asChild>
+          <Button
+            variant="ghost"
+            className="w-auto h-auto p-2 absolute top-2 right-2 text-neutral-600"
+          >
+            <X className="w-4 h-4" />
+          </Button>
+        </PopoverClose>
+        <Button
+          onClick={onAddCard}
+          variant="ghost"
+          className="h-auto w-auto rounded-none p-2 px-5 justify-start font-normal text-sm"
+        >
+          Add card...
+        </Button>
+      </PopoverContent>
+    </Popover>
+  );
 };
