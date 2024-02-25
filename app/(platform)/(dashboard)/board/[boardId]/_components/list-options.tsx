d56718697a6ad7317a665db0aa368c4dd0a14383
Liirus91 <work.liirus@gmail.com>
2024-02-25 09:30:13 +0200

    feat: use action deleteList in ListOptions

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { deleteList } from '@/actions/delete-list';
 import { FormSubmit } from '@/components/form/form-submit';
 import { Button } from '@/components/ui/button';
 import {
@@ -9,8 +10,10 @@ import {
   PopoverTrigger,
 } from '@/components/ui/popover';
 import { Separator } from '@/components/ui/separator';
+import { useAction } from '@/hooks/use-action';
 import { List } from '@prisma/client';
 import { MoreHorizontal, X } from 'lucide-react';
+import { toast } from 'sonner';
 
 interface ListOptionsProps {
   data: List;
@@ -18,6 +21,22 @@ interface ListOptionsProps {
 }
 
 export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
+  const { execute: executeDelete } = useAction(deleteList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title}" deleted`);
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onDelete = (formData: FormData) => {
+    const id = formData.get('id') as string;
+    const boardId = formData.get('boardId') as string;
+
+    executeDelete({ id, boardId });
+  };
+
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -55,7 +74,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
           </FormSubmit>
         </form>
         <Separator />
-        <form>
+        <form action={onDelete}>
           <input hidden name="id" id="id" value={data.id} />
           <input hidden name="boardId" id="boardId" value={data.boardId} />
           <FormSubmit
