2dc13fc5e9f6b23fd4d1b489f7c78c6d4ba845e7
Liirus91 <work.liirus@gmail.com>
2024-02-27 09:15:56 +0200

    feat: add button for adding new card in CardForm

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
 import { forwardRef } from 'react';
 
 interface CardFormProps {
@@ -11,7 +13,19 @@ interface CardFormProps {
 
 export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
   ({ listId, isEditing, enableEditing, disableEditing }, ref) => {
-    return <div>Card Form</div>;
+    return (
+      <div className="pt-2 px-2">
+        <Button
+          onClick={enableEditing}
+          size="sm"
+          variant="ghost"
+          className="h-auto w-full px-2 py-1.5 justify-start text-muted-foreground text-sm"
+        >
+          <Plus className="mr-2 w-4 h-4" />
+          Add card
+        </Button>
+      </div>
+    );
   }
 );
 
