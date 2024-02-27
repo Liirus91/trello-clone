c8724f378c25c9f1eb9829f2d05e6d4d9e0fe162
Liirus91 <work.liirus@gmail.com>
2024-02-27 09:26:47 +0200

    feat: create FormTextarea component

---
+++ components/form/form-textarea.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { KeyboardEventHandler, forwardRef } from 'react';
+
+interface FormTextareaProps {
+  id: string;
+  label?: string;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+  errors?: Record<string, string[]> | undefined;
+  className?: string;
+  onBlur?: () => void;
+  onClick?: () => void;
+  onKeyDown?: KeyboardEventHandler<HTMLTextAreaElement> | undefined;
+  defaultValue?: string;
+}
+
+export const FormTextarea = forwardRef<HTMLTextAreaElement, FormTextareaProps>(
+  (
+    {
+      id,
+      label,
+      placeholder,
+      required,
+      disabled,
+      errors,
+      className,
+      onBlur,
+      onClick,
+      onKeyDown,
+      defaultValue,
+    },
+    ref
+  ) => {
+    return <div>Form textarea</div>;
+  }
+);
+
+FormTextarea.displayName = 'FormTextarea';
