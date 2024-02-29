ca3010320fbc91311bc62eb0468ea279bfcc7ef5
Liirus91 <work.liirus@gmail.com>
2024-02-29 10:35:41 +0200

    feat: add layouts for CardItem

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -8,5 +8,9 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ index, data }: CardItemProps) => {
-  return <div>Card item</div>;
+  return (
+    <div className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm">
+      {data.title}
+    </div>
+  );
 };
