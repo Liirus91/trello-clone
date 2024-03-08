42f8d83ecf52320d49f58fba5921ed8e2d05bdc2
Liirus91 <work.liirus@gmail.com>
2024-03-08 09:52:05 +0200

    feat: use updateCard action on CardModal

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -1,12 +1,15 @@
 'use client';
 
+import { updateCard } from '@/actions/update-card';
 import { FormInput } from '@/components/form/form-input';
 import { Skeleton } from '@/components/ui/skeleton';
+import { useAction } from '@/hooks/use-action';
 import { CardWithList } from '@/types';
 import { useQueryClient } from '@tanstack/react-query';
 import { Layout } from 'lucide-react';
 import { useParams } from 'next/navigation';
 import { ElementRef, useRef, useState } from 'react';
+import { toast } from 'sonner';
 
 interface HeaderProps {
   data: CardWithList;
@@ -16,6 +19,18 @@ export const Header = ({ data }: HeaderProps) => {
   const queryClient = useQueryClient();
   const params = useParams();
 
+  const { execute } = useAction(updateCard, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ['card', data.id] });
+
+      toast.success(`Renamed to "${data.title}"`);
+      setTitle(data.title);
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
   const inputRef = useRef<ElementRef<'input'>>(null);
   const [title, setTitle] = useState(data.title);
 
@@ -23,7 +38,16 @@ export const Header = ({ data }: HeaderProps) => {
     inputRef.current?.form?.requestSubmit();
   };
 
-  const onSubmit = (formData: FormData) => {};
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string;
+    const boardId = params.boardId as string;
+
+    if (title === data.title) {
+      return;
+    }
+
+    execute({ title, boardId, id: data.id });
+  };
 
   return (
     <div className="flex items-start gap-x-3 mb-6 w-full">
