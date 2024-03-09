7812bbe1bb569ab05aebfea2c2f86541f60353e4
Liirus91 <work.liirus@gmail.com>
2024-03-09 11:52:18 +0200

    feat: use updateCard action on Description

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -1,14 +1,17 @@
 'use client';
 
+import { updateCard } from '@/actions/update-card';
 import { FormSubmit } from '@/components/form/form-submit';
 import { FormTextarea } from '@/components/form/form-textarea';
 import { Button } from '@/components/ui/button';
 import { Skeleton } from '@/components/ui/skeleton';
+import { useAction } from '@/hooks/use-action';
 import { CardWithList } from '@/types';
 import { useQueryClient } from '@tanstack/react-query';
 import { AlignLeft } from 'lucide-react';
 import { useParams } from 'next/navigation';
 import { ElementRef, useRef, useState } from 'react';
+import { toast } from 'sonner';
 import { useEventListener, useOnClickOutside } from 'usehooks-ts';
 
 interface DescriptionProps {
@@ -44,11 +47,19 @@ export const Description = ({ data }: DescriptionProps) => {
   useEventListener('keydown', onKeyDown);
   useOnClickOutside(formRef, disableEditing);
 
+  const { execute, fieldErrors } = useAction(updateCard, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ['card', data.id] });
+      toast.success(`Card "${data.title} updated"`);
+    },
+    onError: (error) => toast.error(error),
+  });
+
   const onSubmit = (formData: FormData) => {
     const description = formData.get('description') as string;
     const boardId = params.boardId as string;
 
-    //TODO: add execute
+    execute({ id: data.id, description, boardId });
   };
 
   return (
@@ -57,12 +68,13 @@ export const Description = ({ data }: DescriptionProps) => {
       <div className="w-full">
         <p className="font-semibold text-neutral-700 mb-2">Description</p>
         {isEditing ? (
-          <form ref={formRef} className="space-y-2">
+          <form action={onSubmit} ref={formRef} className="space-y-2">
             <FormTextarea
               id="description"
               className="mr-2 w-full"
               defaultValue={data.description || undefined}
               placeholder="Add a more detailed description..."
+              errors={fieldErrors}
             />
             <div className="flex items-center gap-x-2">
               <FormSubmit>Save</FormSubmit>
