64e0b10a08f20100bfb130302f08371ed1a6c5f6
Liirus91 <work.liirus@gmail.com>
2024-03-13 10:42:49 +0200

    feat: add layouts for Activity

--- components/modals/card-modal/activity.tsx
+++ components/modals/card-modal/activity.tsx
@@ -2,13 +2,26 @@
 
 import { Skeleton } from '@/components/ui/skeleton';
 import { AuditLog } from '@prisma/client';
+import { ActivityIcon } from 'lucide-react';
 
 interface ActivityProps {
   items: AuditLog[];
 }
 
 export const Activity = ({ items }: ActivityProps) => {
-  return <div>Activity</div>;
+  return (
+    <div className="flex items-start gap-x-3 w-full">
+      <ActivityIcon className="w-5 h-5 mt-0.5 text-neutral-700" />
+      <div className="w-full">
+        <p className="font-semibold text-neutral-700 mb-2">Activity</p>
+        <ol className="mt-2 space-y-4">
+          {items.map((item) => (
+            <p>{item.entityTitle}</p>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
 };
 
 Activity.Skeleton = function ActivitySkeleton() {
