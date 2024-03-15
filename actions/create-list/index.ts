e1dacfae22681f870a047ab74888557005ee2b43
Liirus91 <work.liirus@gmail.com>
2024-03-15 10:14:47 +0200

    feat: add createAuditLog in action createList

--- actions/create-list/index.ts
+++ actions/create-list/index.ts
@@ -6,6 +6,8 @@ import { db } from '@/lib/db';
 import { revalidatePath } from 'next/cache';
 import { createSafeAction } from '@/lib/create-safe-action';
 import { CreateList } from './schema';
+import { createAuditLog } from '@/lib/create-audit-log';
+import { ENTITY_TYPE, ACTION } from '@prisma/client';
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -33,6 +35,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     const newOrder = lastList ? lastList.order + 1 : 1;
 
     list = await db.list.create({ data: { title, boardId, order: newOrder } });
+
+    await createAuditLog({
+      entityTitle: list.title,
+      entityId: list.id,
+      entityType: ENTITY_TYPE.LIST,
+      action: ACTION.CREATE,
+    });
   } catch (error) {
     return { error: 'Failed to create' };
   }
