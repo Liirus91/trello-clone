852da746c5f18f61cd0902c3886306351cb23801
Liirus91 <work.liirus@gmail.com>
2024-03-17 09:01:50 +0200

    feat: create ActivityPage

---
+++ app/(platform)/(dashboard)/organization/[organizationid]/activity/page.tsx
@@ -0,0 +1,13 @@
+import { Separator } from '@/components/ui/separator';
+import { Info } from '../_components/info';
+
+const ActivityPage = () => {
+  return (
+    <div className="w-full">
+      <Info />
+      <Separator className="my-2" />
+    </div>
+  );
+};
+
+export default ActivityPage;
