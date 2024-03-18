b9d5f15e8efc9788480fcde65af83c743d707da1
Liirus91 <work.liirus@gmail.com>
2024-03-18 10:12:51 +0200

    feat: show how much boards available in BoardList

--- app/(platform)/(dashboard)/organization/[organizationid]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationid]/_components/board-list.tsx
@@ -1,7 +1,9 @@
 import { FormPopover } from '@/components/form/form-popover';
 import { Hint } from '@/components/hint';
 import { Skeleton } from '@/components/ui/skeleton';
+import { MAX_FREE_BOARDS } from '@/constants/boards';
 import { db } from '@/lib/db';
+import { getAvailableCount } from '@/lib/org-limit';
 import { auth } from '@clerk/nextjs';
 import { HelpCircle, User2 } from 'lucide-react';
 import Link from 'next/link';
@@ -18,6 +20,8 @@ export const BoardList = async () => {
     where: { orgId },
     orderBy: { createdAt: 'desc' },
   });
+
+  const availableCount = await getAvailableCount();
 
   return (
     <div className="space-y-4">
@@ -43,7 +47,9 @@ export const BoardList = async () => {
             role="button"
           >
             <p className="text-sm">Create new board</p>
-            <span className="text-xs">5 remaining</span>
+            <span className="text-xs">{`${
+              MAX_FREE_BOARDS - availableCount
+            } remaning`}</span>
             <Hint
               sideOffset={40}
               description={`Free Workspaces have up to 5 open boards. For unlimited boards upgrade this workspace`}
